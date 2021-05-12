c8c0d7a8c9005992e6699a04dd9d306685f37579
Derek Cruz <Derekcruz94@gmail.com>
2021-05-12 10:53:53 -0700

    Completed array-manipulation-3.

---
+++ array-manipulation-3/src/intersection.js
@@ -0,0 +1,12 @@
+/* exported intersection */
+function intersection(first, second) {
+  var newArr = [];
+  for (var firstUnique of first) {
+    for (var secondUnique of second) {
+      if (firstUnique === secondUnique && !newArr.includes(secondUnique)) {
+        newArr.push(secondUnique);
+      }
+    }
+  }
+  return newArr;
+}
