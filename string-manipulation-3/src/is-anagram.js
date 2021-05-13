907eca0408ed05fca3c510d07984d60fe758379b
Derek Cruz <Derekcruz94@gmail.com>
2021-05-13 12:00:04 -0700

    Completed string-manipulation-3.

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,14 @@
+/* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  firstString = firstString.replaceAll(' ', '');
+  secondString = secondString.replaceAll(' ', '');
+  if (firstString.length !== secondString.length) {
+    return false;
+  }
+  for (var i = 0; i < secondString.length; i++) {
+    if (!firstString.includes(secondString[i])) {
+      return false;
+    }
+  }
+  return true;
+}
